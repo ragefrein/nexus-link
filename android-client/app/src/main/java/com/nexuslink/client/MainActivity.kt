f56a9590b1004ecf2343336a277c532fd3112c89
ragefrein <crlrge01@gmail.com>
2026-06-27 13:36:07 +0700

    UDP TCP Connect to PC and Android

--- android-client/app/src/main/java/com/nexuslink/client/MainActivity.kt
+++ android-client/app/src/main/java/com/nexuslink/client/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.nexuslink.client
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.Socket
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        thread { temukanPcDanHubungkan() }
+    }
+
+    private fun temukanPcDanHubungkan() {
+        try {
+            Log.d("NEXUS", "Mulai mencari PC via UDP...")
+            val udpSocket = DatagramSocket()
+            udpSocket.broadcast = true
+            udpSocket.soTimeout = 10000 
+            val pesan = "NEXUS_DISCOVER".toByteArray()
+            val broadcastAddress = InetAddress.getByName("255.255.255.255")
+            val paketKirim = DatagramPacket(pesan, pesan.size, broadcastAddress, 5051)
+            udpSocket.send(paketKirim)
+            val buffer = ByteArray(1024)
+            val paketTerima = DatagramPacket(buffer, buffer.size)
+            udpSocket.receive(paketTerima) 
+
+            val balasan = String(paketTerima.data, 0, paketTerima.length)
+            val ipPc = paketTerima.address.hostAddress
+
+            Log.d("NEXUS", "Dapat balasan: '$balasan' dari IP PC: $ipPc")
+            udpSocket.close()
+            if (balasan == "NEXUS_SERVER_HERE" && ipPc != null) {
+                Log.d("NEXUS", "Berhasil menemukan PC! Menyambungkan TCP...")
+                val tcpSocket = Socket(ipPc, 5050)
+
+                if (tcpSocket.isConnected) {
+                    Log.d("NEXUS", "HORE! Sukses terhubung ke PC!")
+                    tcpSocket.close()
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("NEXUS", "Terjadi error jaringan: ${e.message}")
+            e.printStackTrace()
+        }
     }
 }
--- pc-server/main.cpp
+++ pc-server/main.cpp
@@ -1,6 +1,78 @@
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sockaddr_in udpAddr;
+    udpAddr.sin_family = AF_INET;
+    udpAddr.sin_addr.s_addr = INADDR_ANY;
+    udpAddr.sin_port = htons(5051);
+    bind(udpSocket, (sockaddr *)&udpAddr, sizeof(udpAddr));
+    SOCKET tcpSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sockaddr_in tcpAddr;
+    tcpAddr.sin_family = AF_INET;
+    tcpAddr.sin_addr.s_addr = INADDR_ANY;
+    tcpAddr.sin_port = htons(5050);
+    bind(tcpSocket, (sockaddr *)&tcpAddr, sizeof(tcpAddr));
+    listen(tcpSocket, SOMAXCONN);
+
+    std::cout << "[SERVER] Menunggu koneksi(UDP 5051) dan koneksi utama (TCP 5050)...\n";
+
+    char buffer[1024];
+    while (true)
+    {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(udpSocket, &readfds);
+        FD_SET(tcpSocket, &readfds);
+        int activity = select(0, &readfds, NULL, NULL, NULL);
+
+        if (activity == SOCKET_ERROR)
+        {
+            std::cerr << "Error pada Radar jaringan.\n";
+            break;
+        }
+        if (FD_ISSET(udpSocket, &readfds))
+        {
+            sockaddr_in clientAddr;
+            int clientAddrSize = sizeof(clientAddr);
+            int bytesReceived = recvfrom(udpSocket, buffer, sizeof(buffer) - 1, 0, (sockaddr *)&clientAddr, &clientAddrSize);
+
+            if (bytesReceived > 0)
+            {
+                buffer[bytesReceived] = '\0';
+                std::string message(buffer);
+
+                if (message == "NEXUS_DISCOVER")
+                {
+                    std::string clientIp = inet_ntoa(clientAddr.sin_addr);
+                    std::cout << "[UDP] HP Android mencari PC dari IP: " << clientIp << std::endl;
+
+                    std::string reply = "NEXUS_SERVER_HERE";
+                    sendto(udpSocket, reply.c_str(), reply.length(), 0, (sockaddr *)&clientAddr, clientAddrSize);
+                }
+            }
+        }
+        if (FD_ISSET(tcpSocket, &readfds))
+        {
+            SOCKET clientSocket = accept(tcpSocket, NULL, NULL);
+            if (clientSocket != INVALID_SOCKET)
+            {
+                std::cout << "HP Android berhasil tersambung secara otomatis!\n";
+                closesocket(clientSocket);
+            }
+        }
+    }
+
+    closesocket(udpSocket);
+    closesocket(tcpSocket);
+    WSACleanup();
     return 0;
 }
