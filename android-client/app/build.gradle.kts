acf8aff1d56338f0d3f9f27acf863e2796b31200
ragefrein <crlrge01@gmail.com>
2026-06-27 12:38:51 +0700

    initial commit

---
+++ android-client/app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.nexuslink.client"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.nexuslink.client"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.10.1")
+}
